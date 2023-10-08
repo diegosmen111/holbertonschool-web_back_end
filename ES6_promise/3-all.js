469237fb2d52b373f124cc52a8a6180a0e29602f
diegosmen111 <dbav_10@hotmail.com>
2023-10-08 13:19:03 -0700

    update

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,11 @@
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`body ${userResponse.firstName} ${userResponse.lastName}`);
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((resp) => {
+      const { body } = resp[0];
+      const { firstName, lastName } = resp[1];
+      console.log(`${body} ${firstName} ${lastName}`);
     })
-    .catch((error) => {
-      console.log("Signup system offline");
-    });
+    .catch(() => console.log('Signup system offline'));
 }
-handleProfileSignup();
