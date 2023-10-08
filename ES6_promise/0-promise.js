90027b08ccfb14112f0be3c122cc7bc617c0d65b
diegosmen111 <dbav_10@hotmail.com>
2023-10-08 12:56:43 -0700

    update

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 // En el archivo 0-promise.js
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     // Aquí puedes realizar cualquier lógica o llamada a una API real
     // En este caso, simplemente resolvemos la promesa sin ningún valor
     resolve();
